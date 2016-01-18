90bb35fc987a42c858377c7b158895baad568749
Noah Damiani <info@noahdamiani.com>
2016-01-18 17:58:23 -0500

    Cool! Working geolocating maps

--- js/controllers/maps.js
+++ js/controllers/maps.js
@@ -9,24 +9,37 @@ mapsControllers.config(
 );
 
 mapsControllers.controller('mapController', function ($scope, $state, $http, FireBase) {
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
   $scope.auth = FireBase.authenticate();
   $scope.auth.$onAuth(function(authData) {
-  $scope.currentUser.$loaded().then(function () {
-    $scope.myApartment = FireBase.getObject('/apartments/' + $scope.currentUser.apartmentId);
-    $scope.myApartment.$loaded().then(function(){
-      var address = $scope.myApartment.address;
-      var street = address.street.split(' ').join('+');
-      var city = address.city;
-      var state = 'NY';
-      var mapURL = 'http://maps.google.com/maps/api/geocode/json?address=' + street + ',' + city + ',+' + state + '&sensor=false';
-      console.log(mapURL);
-      $http.get(mapURL).success(function(mapData) {
-        angular.extend($scope, mapData);
-        $scope.map = mapData;
-        console.log(mapData);
+    $scope.currentUser.$loaded().then(function () {
+      $scope.myApartment = FireBase.getObject('/apartments/' + $scope.currentUser.apartmentId);
+      $scope.myApartment.$loaded().then(function(){
+        var address = $scope.myApartment.address;
+        var street = address.street.split(' ').join('+');
+        var city = address.city;
+        var state = 'NY';
+        var mapURL = 'http://maps.google.com/maps/api/geocode/json?address=' + street + ',' + city + ',+' + state + '&sensor=false';
+
+        $http.get(mapURL).success(function(mapData) {
+          $scope.results = mapData.results[0].geometry.location;
+          angular.extend($scope, {
+              map: {
+                  center: {
+                      latitude: $scope.results.lat,
+                      longitude: $scope.results.lng
+                  },
+                  zoom: 18,
+                  markers: [{
+                      id: new Date(),
+                      coords: {
+                          latitude: $scope.results.lat,
+                          longitude: $scope.results.lng
+                      }
+                  }]
+              }
+          });
+        });
       });
     });
   });
-});
 });
